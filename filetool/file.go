8eb9ec836e76780561a544cd88fc0ddbeb2a87d3
zsk <718114245@qq.com>
2019-03-28 11:03:18 +0800

    更新file

--- filetool/file.go
+++ filetool/file.go
@@ -6,29 +6,36 @@ import (
 	"path/filepath"
 )
 
-// 分段获取指定文件的数据包
+// ReadPagingFile 分段获取指定文件的数据包
 // @pagIndex 表示数据包的下标
 // @pagSize 表示一次获取的数据包大小
 // @filePath 表示文件路径
-// @rerurn 读取到的文件数据包  error 表示读取异常
-func ReadPagingFile(pagIndex int, pagSize int, filePath string) ([]byte, error) {
+// @rerurn 读取到的文件数据包 @[]byte 读取到的数据 @int 有效数据长度  @error 表示读取异常
+func ReadPagingFile(pagIndex int, pagSize int, filePath string) ([]byte, int, error) {
+	var resultData []byte   //返回数据
+	var resultDataSize int  //有效数据长度
+	var isRemenberSize bool //是否已记住
 	file, err := os.Open(filePath)
 	if err != nil {
-		return nil, err
+		return nil, resultDataSize, err
 	}
 	defer file.Close()
-
 	bufReader := bufio.NewReader(file)
 	_, err = bufReader.Discard(pagIndex * pagSize) //跳过指定字节数
 	if err != nil {
-		return nil, err
+		return nil, resultDataSize, err
 	}
-	var result []byte
 	for i := 0; i < pagSize; i++ {
-		b, _ := bufReader.ReadByte()
-		result = append(result, b) //读取超出接线默认用0补足
+		b, e := bufReader.ReadByte()
+		if e != nil {
+			if !isRemenberSize {
+				isRemenberSize = true
+				resultDataSize = i
+			}
+		}
+		resultData = append(resultData, b) //读取超出接线默认用0补足
 	}
-	return result, nil
+	return resultData, resultDataSize, nil
 }
 
 //获取文件大小
