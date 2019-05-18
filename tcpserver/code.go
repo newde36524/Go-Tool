516abdba139c51af56aa451f7b5775710ec10651
zsk <718114245@qq.com>
2019-05-18 16:55:37 +0800

    tcpserver 修改状态码

--- tcpserver/code.go
+++ tcpserver/code.go
@@ -1,15 +1,13 @@
 package tcpserver
 
-//TimeOutState 当前状态: 1.发送超时 2.接收超时 3.第一次处理超时 4.处理超时
+//TimeOutState 当前状态: 1.发送超时 2.接收超时 3.处理超时
 type TimeOutState byte
 
 const (
-	//SendTimeOut .
+	//SendTimeOut 发送超时
 	SendTimeOut TimeOutState = 1
-	//RecvTimeOut .
+	//RecvTimeOut 接收超时
 	RecvTimeOut TimeOutState = 2
-	//FirstHandTimeOut .
-	FirstHandTimeOut TimeOutState = 3
-	//HandTimeOut .
-	HandTimeOut TimeOutState = 4
+	//HandTimeOut 处理超时
+	HandTimeOut TimeOutState = 3
 )
