5573c977715fa513cec6e3d3539b0a2f329855ca
zsk <718114245@qq.com>
2020-02-22 15:51:49 +0800

    更新协程池相关

--- BulkRunTool/Call.go
+++ BulkRunTool/Call.go
@@ -265,36 +265,39 @@ type gPool struct {
 	ctx     context.Context
 	taskNum int
 	exp     time.Duration
-	m       map[interface{}]*gItem
+	m       sync.Map
 	sign    chan struct{}
 }
 
-func newgPoll(ctx context.Context, taskNum int, exp time.Duration, size int) gPool {
-	g := gPool{
+func newgPoll(ctx context.Context, perItemTaskNum int, exp time.Duration, parallelSize int) *gPool {
+	g := &gPool{
 		ctx:     ctx,
-		taskNum: taskNum,
+		taskNum: perItemTaskNum,
 		exp:     exp,
-		m:       make(map[interface{}]*gItem),
-		sign:    make(chan struct{}, size),
+		sign:    make(chan struct{}, parallelSize),
 	}
 	return g
 }
 
-func (g gPool) SchduleByKey(key interface{}, task func()) {
-	if v, ok := g.m[key]; ok {
-		v.DoOrInChan(task)
-	} else {
-		select {
-		case g.sign <- struct{}{}:
-		}
-		g.m[key] = newgItem(g.ctx, g.taskNum, g.exp, func() {
-			delete(g.m, key)
+//SchduleByKey 为不同key值下的任务并行调用,相同key值下的任务串行调用,并行任务量和串行任务量由配置参数决定
+func (g *gPool) SchduleByKey(key interface{}, task func()) bool {
+	if v, ok := g.m.Load(key); ok { //希望在同一协程下顺序执行
+		gItem := v.(*gItem)
+		return gItem.DoOrInChan(task)
+	}
+	select {
+	case <-g.ctx.Done():
+		return false
+	case g.sign <- struct{}{}:
+		gItem := newgItem(g.ctx, g.taskNum, g.exp, func() {
+			g.m.Delete(key)
 			select {
 			case <-g.sign:
 			default:
 			}
 		})
-		g.m[key].DoOrInChan(task)
+		g.m.Store(key, gItem)
+		return gItem.DoOrInChan(task)
 	}
 }
 
@@ -309,20 +312,24 @@ type gItem struct {
 func newgItem(ctx context.Context, taskNum int, exp time.Duration, onExit func()) *gItem {
 	return &gItem{
 		tasks:  make(chan func(), taskNum),
-		sign:   make(chan struct{}, 1),
+		sign:   make(chan struct{}, 1), //
 		ctx:    ctx,
 		exp:    exp,
 		onExit: onExit,
 	}
 }
 
-func (g *gItem) DoOrInChan(task func()) {
+func (g *gItem) DoOrInChan(task func()) bool {
 	select {
 	case <-g.ctx.Done():
+		return false
 	case g.tasks <- task:
+		return true
 	case g.sign <- struct{}{}:
 		go g.worker()
-		g.DoOrInChan(task)
+		return g.DoOrInChan(task)
+	default:
+		return false
 	}
 }
 
@@ -345,7 +352,6 @@ func (g *gItem) worker() {
 		case task := <-g.tasks: //执行任务优先
 			//timer.Reset(g.exp)
 			/*
-				这行注释就放在这里
 				1) 如果重置时间,那么会在任务全部处理完成后继续等待过期,虽然空闲等待是一种资源浪费,但这主要用于复用当前协程对任务队列的执行
 				2) 如果不重置时间,那么当前协程会在有效期内执行任务队列,但超过时间后协程只会创建给下一个任务队列
 				3) 个人认为,不重置时间可均衡各个任务队列之间的任务调度
