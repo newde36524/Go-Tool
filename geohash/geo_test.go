2cbb0e4a81969e0171ebf1996ee0aae8ee979d2c
zsk <718114245@qq.com>
2020-04-19 09:43:53 +0800

    更新geohash测试文件

--- geohash/geo_test.go
+++ geohash/geo_test.go
@@ -6,6 +6,12 @@ import (
 )
 
 func TestArround(t *testing.T) {
+	result := Around(116.3967, 44.9999)
+	t.Error(result)
+	fmt.Println(result)
+}
+
+func TestArround2(t *testing.T) {
 	result := Around2(116.3967, 44.9999, 8)
 	t.Error(result)
 	fmt.Println(result)
