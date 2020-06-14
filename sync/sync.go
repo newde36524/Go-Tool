c783c267a36d782bb14ed2c99205be6afcfb8792
zsk <718114245@qq.com>
2020-06-14 21:40:20 +0800

    修改sync

--- sync/sync.go
+++ sync/sync.go
@@ -4,49 +4,67 @@ import (
 	"context"
 	"errors"
 	"sync"
+	"time"
 )
 
 type SyncOp struct {
-	mu sync.Mutex
-	m  map[string]chan interface{}
+	mu    sync.Mutex
+	m     map[string]chan *Entity
+	signs map[string]chan struct{}
 }
 
 func NewSyncOp() *SyncOp {
 	return &SyncOp{
-		m: make(map[string]chan interface{}),
+		m:     make(map[string]chan *Entity),
+		signs: make(map[string]chan struct{}),
 	}
 }
 
-func (s *SyncOp) Do(ctx context.Context, key string, fn func()) (interface{}, error) {
+func (s *SyncOp) Do(timeout time.Duration, key string, fn func()) (interface{}, error) {
 	s.mu.Lock()
-	ch := make(chan interface{})
-	s.m[key] = ch
+	ch, ok := s.m[key]
+	if !ok {
+		ch = make(chan *Entity)
+		s.m[key] = ch
+	}
+	if _, ok := s.signs[key]; !ok {
+		s.signs[key] = make(chan struct{}, 1)
+	}
 	s.mu.Unlock()
-	fn()
+	ctx, _ := context.WithTimeout(context.Background(), timeout)
 	select {
 	case <-ctx.Done():
-		return 0, errors.New("中断操作")
-	case v := <-ch:
-		return v, nil
+		return nil, errors.New("任务超时1")
+	case s.signs[key] <- struct{}{}:
+		go fn()
 	}
+	defer func() {
+		<-s.signs[key]
+	}()
+	select {
+	case <-ctx.Done():
+		return nil, errors.New("任务超时2")
+	case v := <-ch:
+		return v.value, v.err
+	}
+}
+
+type Entity struct {
+	value interface{}
+	err   error
 }
 
 func (s *SyncOp) Back(key string, fn func() (interface{}, error)) (bool, error) {
 	s.mu.Lock()
 	v, ok := s.m[key]
-	delete(s.m, key)
 	s.mu.Unlock()
 	if !ok {
 		return false, nil
 	}
 	value, err := fn()
-	if err != nil {
-		return false, err
+	v <- &Entity{
+		value: value,
+		err:   err,
 	}
-	select {
-	case v <- value:
-		return true, nil
-	default:
-		return false, nil
-	}
+	return true, nil
 }
